c845693519cfcaaf8b28c89a80c153922e103958
SPython <rvan.mega@gmail.com>
2017-09-18 16:24:21 +0500

    Add info to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,21 @@ from setuptools import setup
 
 setup(name="s3m",
       version="1.0.2",
+      py_modules=["s3m"],
       description="sqlite3 wrapper for multi-threaded applications",
       author="Ivan Konovalov",
-      py_modules=["s3m"])
+      author_email="rvan.mega@gmail.com",
+      url="https://github.com/SPython/s3m",
+      license="GPLv3",
+      python_requires=">=3",
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Topic :: Database",
+          "Topic :: Software Development :: Libraries",
+          "Topic :: Software Development :: Libraries :: Python Modules"],
+      keywords="sqlite3 sqlite multithreading thread")
