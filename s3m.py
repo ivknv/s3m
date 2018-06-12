a1b3c7d5795e4899919b96e78696a6407626b102
Ivan Konovalov <ivknv0@gmail.com>
2018-06-12 18:35:57 +0500

    Change license to LGPLv3

--- s3m.py
+++ s3m.py
@@ -9,7 +9,7 @@ S3M - sqlite3 wrapper for multithreaded applications
 # Copyright (C) 2018 Ivan Konovalov
 #
 # This library is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
